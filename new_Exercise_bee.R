0a4652a33084e88f4c1f89fbe2a9a297d07269b9
Enrico Turato <en3817tu@fys.local>
2022-11-23 13:42:04 +0100

    updated

--- new_Exercise_bee.R
+++ new_Exercise_bee.R
@@ -90,3 +90,129 @@ polygon(c(xx, rev(xx)),
         c(y_hat$fit+1.96*y_hat$se.fit,
           rev(y_hat$fit-1.96*y_hat$se.fit)),
         col = rgb(0,1,0,.5), border = FALSE)'
+
+
+###################################################################################################
+###################################################################################################
+###################################################################################################
+#
+#
+#
+# after solution were given during lecture
+#
+#
+#
+###################################################################################################
+###################################################################################################
+###################################################################################################
+library(MASS)
+library(MuMIn)
+dat = read.csv("Eulaema.csv")
+head(dat)
+
+
+m = glm.nb(Eulaema_nigrita ~ MAP + forest., data = dat)
+summary(m)
+
+
+1- m$deviance/m$null.deviance
+
+
+plot(dat$forest., dat$Eulaema_nigrita, col="grey", las=1,
+     xlab="Forest cover",
+     ylab="El. nigrita abundance")
+
+newforest = seq(min(dat$forest.), max(dat$forest.), length.out=200)
+
+newMAP = rep(mean(dat$MAP), length(newforest))
+
+y_hat = predict(m, newdata=list(MAP=newMAP,
+                                forest.=newforest),
+                type="response")
+
+lines(newforest, y_hat,lwd=2)
+
+newMAP2 = rep(mean(dat$MAP)+sd(dat$MAP), length(newforest))
+
+y_hat2 = predict(m, newdata=list(MAP=newMAP2,
+                                 forest.=newforest),
+                 type="response")
+
+newMAP3 = rep(mean(dat$MAP)-sd(dat$MAP), length(newforest))
+
+y_hat3 = predict(m, newdata=list(MAP=newMAP3,
+                                 forest.=newforest),
+                 type="response")
+
+lines(newforest, y_hat2, lwd=2, col=2)
+
+lines(newforest, y_hat3, lwd=2, col=3)
+
+legend("topleft", lty=1, lwd=2, col=1:3, bty="n",
+       legend=c("MAP = Mean",
+                "MAP = Mean + SD",
+                "MAP = Mean - SD"))
+
+#########################################################################
+#########################################################################
+#
+# trying to do a similar plot but for forest
+#
+#########################################################################
+#########################################################################
+
+
+dat_low_forest_coverage = dat[dat$forest.<=0.33,]
+dat_medium_forest_coverage = dat[dat$forest.>0.33 & dat$forest.<=0.66,] 
+dat_high_forest_coverage = dat[dat$forest.>0.66,]
+
+plot(dat$MAP, dat$Eulaema_nigrita, col="grey", las=1,
+     xlab="MAP",
+     ylab="El. nigrita abundance")
+
+dat_low_forest_coverage = dat[dat$forest.<=0.33,]
+length(dat_low_forest_coverage$forest.)
+dat_medium_forest_coverage = dat[dat$forest.>0.33 & dat$forest.<=0.66,] 
+length(dat_medium_forest_coverage$forest.)
+dat_high_forest_coverage = dat[dat$forest.>0.66,]
+length(dat_high_forest_coverage$forest.)
+
+
+newMAP_low = seq(min(dat_low_forest_coverage$MAP), max(dat_low_forest_coverage$MAP), length.out=200)
+
+newforest_low = rep(mean(dat_low_forest_coverage$forest.), length(newMAP_low))
+
+newMAP_medium = seq(min(dat_medium_forest_coverage$MAP), max(dat_medium_forest_coverage$MAP), length.out=200)
+
+newforest_medium = rep(mean(dat_medium_forest_coverage$forest.), length(newMAP_medium))
+
+newMAP_high = seq(min(dat_high_forest_coverage$MAP), max(dat_high_forest_coverage$MAP), length.out=200)
+
+newforest_high = rep(mean(dat_high_forest_coverage$forest.), length(newMAP_high))
+
+y_hat = predict(m, newdata=list(MAP=newMAP_low,
+                                forest.=newforest_low),
+                type="response")
+
+lines(newMAP_low, y_hat,lwd=2)
+
+
+
+y_hat2 = predict(m, newdata=list(MAP=newMAP_medium,
+                                 forest.=newforest_medium),
+                 type="response")
+
+
+
+y_hat3 = predict(m, newdata=list(MAP=newMAP_high,
+                                 forest.=newforest_high),
+                 type="response")
+
+lines(newMAP_medium, y_hat2, lwd=2, col=2)
+
+lines(newMAP_high, y_hat3, lwd=2, col=3)
+
+legend("topleft", lty=1, lwd=2, col=1:3, bty="n",
+       legend=c("forest_coverage = low",
+                "forest_coverage = medium",
+                "forest_coverage = high"))
